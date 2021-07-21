0c259d88e1a412eb5dbcfaa913b131c437db07bf
Robert Prellwitz <robprellwitz@uwalumni.com>
2021-07-21 06:43:36 -0500

    shell

--- src/PingClient.java
+++ src/PingClient.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class PingClient {
+import java.net.Inet4Address;
+import java.net.InetAddress;
+
+public class PingClient extends UDPPinger{
+
+    public PingMessage(InetAddress adr, int port, String payload){}
+
+    public InetAddress getIP(){
+        Inet4Address ip=0.0.0.0;
+        return ip;
+    }
+
+    public int getPort() {
+        int port =0;
+        return port;
+    }
+
+    public String getPayload(){
+        String payload = "";
+        return payload;
+    }
+
+    run();
 }
