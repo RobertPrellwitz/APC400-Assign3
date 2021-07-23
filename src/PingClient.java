3975decb6000b0071127dd6c5ad6bbb3c05237a1
Robert Prellwitz <robert.prellwitz@summaft.com>
2021-07-23 15:03:21 -0500

    get duration or send time and print data

--- src/PingClient.java
+++ src/PingClient.java
@@ -2,14 +2,20 @@ import java.io.IOException;
 import java.net.*;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 public class PingClient extends UDPPinger{
+    Date now = new Date();
+    LocalDateTime sentTime;
+    LocalDateTime recieveTime;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         PingClient pinger = new PingClient();
         pinger.run();
     }
-    public void run () {
+    public void run () throws Exception {
 
         DatagramSocket pingSock = null;
         byte [] buff;
@@ -33,16 +39,24 @@ public class PingClient extends UDPPinger{
             buff = payLoad.getBytes();
             outPacket = new DatagramPacket(buff, buff.length,message.internet,message.portNumber);
             try{
-            pingSock.send(outPacket);}
+            pingSock.send(outPacket);
+            sentTime = LocalDateTime.now();
+            System.out.println(sentTime);
+            }
             catch(IOException exp){
                 System.out.println("Recieve Ping: " + exp);
             }
             inPacket = new DatagramPacket(buff, buff.length);
+            pingSock.receive(inPacket);
+            recieveTime = LocalDateTime.now();
+            System.out.println(recieveTime);
             String Data = inPacket.getData().toString();
             String Address = inPacket.getAddress().toString();
             int Port = inPacket.getPort();
             System.out.println("Recieved Packet " + Data + " from: " + Address + "on port: " + Port  );
-            ping.receivePing();
+            Duration RTT = Duration.between(sentTime , recieveTime);
+            long milliSeconds = RTT.toMillis();
+            message.printData(inPacket, milliSeconds);
 
         }
 
